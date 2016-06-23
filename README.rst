06d47289ed76c0fcbceda256d37cf4eced86decd
Luke Maxwell <lukemaxwellshouse@gmail.com>
2016-06-23 22:28:31 +0100

    Packing wip.

---
+++ README.rst
@@ -0,0 +1,20 @@
+==========
+Proxytools
+==========
+
+A command line tool for finding and testing public web proxies.
+
+Installation
+^^^^^^^^^^^^
+.. code-block:: bash 
+
+   apt-get update
+   apt-get install -y curl python-dev libcurl3-openssl-dev \
+                      libxml2-dev libxslt1-dev \
+                      zlib1g-dev libffi-dev libssl-dev
+   
+   
+   export PYCURL_SSL_LIBRARY=openssl
+   git clone https://github.com/lukemaxwell/proxytools.git
+   cd proxytools
+   python setup.py install
---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+#with open('LICENSE') as f:
+#    license = f.read()
+
+setup(
+    name='proxytools',
+    version='0.0.1',
+    description='Search and test public web proxies',
+    long_description=readme,
+    author='Luke Maxwell',
+    author_email='luke@codepunk.xyz',
+    url='',
+    #license=license,
+    packages=['proxytools'],
+    entry_points = {
+        'console_scripts': ['proxytools=proxytools.proxytools:cli'],
+    },
+    install_requires=[
+        'backports-abc>=0.4',
+        'click>=6.6',
+        'lxml>=3.6.0',
+        'pycurl>=7.43.0',
+        'tornado>=4.3',
+    ],
+    # packages=find_packages(exclude=('tests', 'docs'))
+)
+
